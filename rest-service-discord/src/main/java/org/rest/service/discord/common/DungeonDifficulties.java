4ac8d0716bde7de761d0b4b619119e9e83c29f06
Zuron <Sleoniy@live.com>
2020-07-09 22:01:40 -0400

    Removing Discord Module, adding Framework Module

--- rest-service-discord/src/main/java/org/rest/service/discord/common/DungeonDifficulties.java
+++
@@ -1,5 +0,0 @@
-package org.rest.service.discord.common;
-
-public enum DungeonDifficulties {
-  DEFAULT, ADVENTURE, APOCALYPSE;
-}
--- rest-service-discord/src/main/java/org/rest/service/discord/common/UtiLittyConstants.java
+++
@@ -1,9 +0,0 @@
-package org.rest.service.discord.common;
-
-public class UtiLittyConstants {
-  public final static String UTILITTY = "UtiLitty";
-  public final static String UTILITTY_ID =
-      "NzExNzI4NTUxNTU2MDIyMzIy.XsHO9g.VrQj5XRSOjwRHdOklLZAkuYHFxA";
-
-  private UtiLittyConstants() {}
-}
